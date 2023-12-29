42a4e0457b13e18b0092b96d77613a15357bdd89
JongKwonYou <yjk.dev.9866@healthport.co.kr>
2023-12-29 16:52:24 +0900

    feat - 디자인 시스템 ( Buttons 추가 )

--- lib/views/design/ui.dart
+++ lib/views/design/ui.dart
@@ -14,12 +14,13 @@ class DesignSystemUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> children = [
-      const Actions(),
+      const Buttons(),
     ];
 
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: children,
       ),
     );
@@ -39,97 +40,410 @@ class DesignSystemUI extends StatelessWidget {
   }
 }
 
-/// Actions
-class Actions extends StatelessWidget {
-  const Actions({super.key});
+/// Cards
+
+/// Dividers
+
+/// Text Field ( Input )
+
+/// Buttons
+class Buttons extends StatefulWidget {
+  const Buttons({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends State<Buttons> {
+  bool isStandardIconSelected = false;
+  bool isFilledIconSelected = false;
+  bool isOutlinedIconSelected = false;
 
   @override
   Widget build(BuildContext context) {
-    return const ComponentGroupDecoration(
-        title: "Actions", children: <Widget>[]);
+    return ComponentGroupDecoration(title: "Buttons", children: <Widget>[
+      createTextButtons(),
+      verticalPadding,
+      createElevatedButtons(),
+      verticalPadding,
+      createFilledButtons(),
+      verticalPadding,
+      createOutlinedButtons(),
+      verticalPadding,
+      createFABButtons(),
+      verticalPadding,
+      createIconButtons(),
+      verticalPadding,
+      const SingleChoice(),
+      verticalPadding,
+    ]);
+  }
+
+  Widget createTextButtons() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          const Text(
+            "Text Button :",
+            style: TextStyle(color: Color(0xff333333), fontSize: 12),
+          ),
+          horizontalPadding,
+          createTextButton("Text", onPressed: () {}),
+          horizontalPadding,
+          createTextButton("Text"),
+          horizontalPadding,
+          createTextButton("Text",
+              icon: const Icon(Icons.add), onPressed: () {}),
+          horizontalPadding,
+          createTextButton("Text", icon: const Icon(Icons.add)),
+        ],
+      ),
+    );
+  }
+
+  Widget createElevatedButtons() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          const Text(
+            "Elevated Button :",
+            style: TextStyle(color: Color(0xff333333), fontSize: 12),
+          ),
+          horizontalPadding,
+          createElevatedButton("Elevated", onPressed: () {}),
+          horizontalPadding,
+          createElevatedButton("Elevated"),
+          horizontalPadding,
+          createElevatedButton("Elevated",
+              icon: const Icon(Icons.add), onPressed: () {}),
+          horizontalPadding,
+          createElevatedButton("Elevated", icon: const Icon(Icons.add)),
+        ],
+      ),
+    );
+  }
+
+  Widget createFilledButtons() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          const Text(
+            "Filled Button :",
+            style: TextStyle(color: Color(0xff333333), fontSize: 12),
+          ),
+          horizontalPadding,
+          createFilledButton("Filled", onPressed: () {}),
+          horizontalPadding,
+          createFilledButton("Filled"),
+          horizontalPadding,
+          createFilledButton("Filled",
+              icon: const Icon(Icons.add), onPressed: () {}),
+          horizontalPadding,
+          createFilledButton("Filled", icon: const Icon(Icons.add)),
+        ],
+      ),
+    );
+  }
+
+  Widget createOutlinedButtons() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          const Text(
+            "Outlined Button :",
+            style: TextStyle(color: Color(0xff333333), fontSize: 12),
+          ),
+          horizontalPadding,
+          createOutlineButton("Outlined", onPressed: () {}),
+          horizontalPadding,
+          createOutlineButton("Outlined"),
+          horizontalPadding,
+          createOutlineButton("Outlined",
+              icon: const Icon(Icons.add), onPressed: () {}),
+          horizontalPadding,
+          createOutlineButton("Outlined", icon: const Icon(Icons.add)),
+        ],
+      ),
+    );
+  }
+
+  Widget createFABButtons() {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                const Text(
+                  "fab Button :",
+                  style: TextStyle(color: Color(0xff333333), fontSize: 12),
+                ),
+                horizontalPadding,
+                createFloatingActionButton(const Icon(Icons.add), () {}),
+                horizontalPadding,
+                createFloatingActionButton(const Icon(Icons.add), () {},
+                    label: "Extended", type: FAB_TYPE.EXTENDED),
+                horizontalPadding,
+                createFloatingActionButton(const Icon(Icons.add), () {},
+                    type: FAB_TYPE.STANDARD),
+                horizontalPadding,
+                createFloatingActionButton(const Icon(Icons.add), () {},
+                    type: FAB_TYPE.LARGE),
+              ],
+            )));
+  }
+
+  Widget createIconButtons() {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text(
+              "Icon Button :",
+              style: TextStyle(color: Color(0xff333333), fontSize: 12),
+            ),
+            horizontalPadding,
+            Column(
+              children: [
+                createIconButton(const Icon(Icons.settings_outlined), () {
+                  setState(() {
+                    isStandardIconSelected = !isStandardIconSelected;
+                  });
+                },
+                    isSelected: isStandardIconSelected,
+                    selectedIcon: const Icon(Icons.settings)),
+                createIconButton(const Icon(Icons.settings_outlined), null,
+                    isSelected: isStandardIconSelected,
+                    selectedIcon: const Icon(Icons.settings)),
+              ],
+            ),
+            horizontalPadding,
+            Column(
+              children: [
+                createIconButton(const Icon(Icons.settings_outlined), () {
+                  setState(() {
+                    isFilledIconSelected = !isFilledIconSelected;
+                  });
+                },
+                    isSelected: isFilledIconSelected,
+                    selectedIcon: const Icon(Icons.settings),
+                    type: IconButtonType.FILLED),
+                createIconButton(const Icon(Icons.settings_outlined), null,
+                    isSelected: isFilledIconSelected,
+                    selectedIcon: const Icon(Icons.settings),
+                    type: IconButtonType.FILLED)
+              ],
+            ),
+            horizontalPadding,
+            Column(
+              children: [
+                createIconButton(const Icon(Icons.settings_outlined), () {
+                  setState(() {
+                    isOutlinedIconSelected = !isOutlinedIconSelected;
+                  });
+                },
+                    isSelected: isOutlinedIconSelected,
+                    selectedIcon: const Icon(Icons.settings),
+                    type: IconButtonType.OUTLINED),
+                createIconButton(const Icon(Icons.settings_outlined), null,
+                    isSelected: isOutlinedIconSelected,
+                    selectedIcon: const Icon(Icons.settings),
+                    type: IconButtonType.OUTLINED)
+              ],
+            )
+          ],
+        ));
   }
 }
 
-/**
- * Cards
- */
+// Text Button
+Widget createTextButton(String text, {Icon? icon, VoidCallback? onPressed}) {
+  if (icon != null) {
+    return TextButton.icon(onPressed: onPressed, icon: icon, label: Text(text));
+  } else {
+    return TextButton(onPressed: onPressed, child: Text(text));
+  }
+}
 
-/**
- * Dividers
- */
+// Elevated Button
+Widget createElevatedButton(String text,
+    {Icon? icon, VoidCallback? onPressed}) {
+  if (icon != null) {
+    return ElevatedButton.icon(
+        onPressed: onPressed, icon: icon, label: Text(text));
+  } else {
+    return ElevatedButton(onPressed: onPressed, child: Text(text));
+  }
+}
 
-/**
- * Text Field ( Input )
- */
+// Filled Button
+Widget createFilledButton(String text, {Icon? icon, VoidCallback? onPressed}) {
+  if (icon != null) {
+    return FilledButton.icon(
+        onPressed: onPressed, icon: icon, label: Text(text));
+  } else {
+    return FilledButton(onPressed: onPressed, child: Text(text));
+  }
+}
 
-/**
- * Buttons
- */
+// Outline Button
+Widget createOutlineButton(String text, {Icon? icon, VoidCallback? onPressed}) {
+  if (icon != null) {
+    return OutlinedButton.icon(
+        onPressed: onPressed, icon: icon, label: Text(text));
+  } else {
+    return OutlinedButton(onPressed: onPressed, child: Text(text));
+  }
+}
 
-/**
- * Radio buttons
- */
+// Floating Action Button
+enum FAB_TYPE { SMALL, EXTENDED, STANDARD, LARGE }
 
-/**
- * Sliders
- */
+Widget createFloatingActionButton(Icon icon, VoidCallback onPressed,
+    {String? label, FAB_TYPE? type}) {
+  if (type == FAB_TYPE.SMALL) {
+    return FloatingActionButton.small(onPressed: onPressed, child: icon);
+  } else if (type == FAB_TYPE.STANDARD) {
+    return FloatingActionButton(onPressed: onPressed, child: icon);
+  } else if (type == FAB_TYPE.LARGE) {
+    return FloatingActionButton.large(onPressed: onPressed, child: icon);
+  } else if (type == FAB_TYPE.EXTENDED) {
+    return FloatingActionButton.extended(
+      onPressed: onPressed,
+      icon: icon,
+      label: label == null ? const Spacer() : Text(label),
+    );
+  }
+  return FloatingActionButton.small(
+    onPressed: onPressed,
+    child: icon,
+  );
+}
 
-/**
- * Switches
- */
+// Icon Button
+enum IconButtonType { FILLED, OUTLINED, STANDARD }
 
-/**
- * Progress
- */
+Widget createIconButton(Icon icon, VoidCallback? onPressed,
+    {bool isSelected = false,
+    Icon? selectedIcon,
+    IconButtonType type = IconButtonType.STANDARD}) {
+  if (type == IconButtonType.FILLED) {
+    return IconButton.filled(
+      isSelected: isSelected,
+      icon: icon,
+      selectedIcon: selectedIcon,
+      onPressed: onPressed,
+    );
+  } else if (type == IconButtonType.OUTLINED) {
+    return IconButton.outlined(
+      isSelected: isSelected,
+      icon: icon,
+      selectedIcon: selectedIcon,
+      onPressed: onPressed,
+    );
+  } else {
+    return IconButton(
+      isSelected: isSelected,
+      icon: icon,
+      selectedIcon: selectedIcon,
+      onPressed: onPressed,
+    );
+  }
+}
 
-/**
- * Checkboxes
- */
+enum SegmentSingle { Day, Week, Month, Year }
 
-/**
- * Chips
- */
+// Segmented Button
+class SingleChoice extends StatefulWidget {
+  const SingleChoice({super.key});
 
-/**
- * Tab Bar
- */
+  @override
+  State<StatefulWidget> createState() => _SingleChoiceState();
+}
 
-/**
- * App Bar
- */
+class _SingleChoiceState extends State<SingleChoice> {
+  SegmentSingle data = SegmentSingle.Day;
 
-/**
- * SnackBar
- */
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<SegmentSingle>(
+        segments: const <ButtonSegment<SegmentSingle>>[
+          ButtonSegment(
+              value: SegmentSingle.Day,
+              label: Text('Day'),
+              icon: Icon(Icons.calendar_view_day)),
+          ButtonSegment(
+              value: SegmentSingle.Week,
+              label: Text('Week'),
+              icon: Icon(Icons.calendar_view_week)),
+          ButtonSegment(
+              value: SegmentSingle.Month,
+              label: Text('Month'),
+              icon: Icon(Icons.calendar_view_month)),
+          ButtonSegment(
+              value: SegmentSingle.Year,
+              label: Text('Year'),
+              icon: Icon(Icons.calendar_today)),
+        ],
+        selected: <SegmentSingle>{
+          data
+        },
+        onSelectionChanged: (select) {
+          setState(() {
+            data = select.first;
+          });
+        },
+      style: ButtonStyle(
+        // backgroundColor: MaterialStateProperty.resolveWith((states) => Color(0x00000000)),
+        // overlayColor: MaterialStateProperty.resolveWith((states) => Theme.of(context).colorScheme.surface),
+        surfaceTintColor: MaterialStateProperty.resolveWith((states) => Color(0xffffffff)),
+        overlayColor: MaterialStateProperty.resolveWith((states) => Color(0xff30C6C0)),
+        // shape: MaterialStateProperty.resolveWith((states) => ),
+        elevation: MaterialStateProperty.resolveWith((states) => 10.0),
 
-/**
- * Bottom sheet
- */
+      )
+    );
+  }
+}
 
-/**
- * Dialog
- */
+/// Radio buttons
 
-/**
- * Menus ( Drop Down )
- */
+/// Sliders
 
-/**
- * Date picker
- */
+/// Switches
 
-/**
- * Time picker
- */
+/// Progress
 
-/**
- * Navigation drawer
- */
+/// Checkboxes
+
+/// Chips
+
+/// Tab Bar
+
+/// App Bar
+
+/// SnackBar
+
+/// Bottom sheet
+
+/// Dialog
+
+/// Menus ( Drop Down )
+
+/// Date picker
+
+/// Time picker
+
+/// Navigation drawer
 
 /// Component Group Decoration
 /// * TODO
 /// * - FocusTraversalGroup 뭔지 알아볼 것
-/// * - Theme.of(context).colorScheme, textTheme 값 변경 가능한지?
 class ComponentGroupDecoration extends StatelessWidget {
   const ComponentGroupDecoration(
       {super.key, required this.title, required this.children});
@@ -142,18 +456,17 @@ class ComponentGroupDecoration extends StatelessWidget {
     return FocusTraversalGroup(
         child: Card(
       margin: EdgeInsets.zero,
-      elevation: 3,
+      elevation: 0,
       color: Theme.of(context).colorScheme.surfaceVariant.withOpacity(0.5),
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0),
-        child: Center(
-          child: Column(
-            children: [
-              Text(title, style: Theme.of(context).textTheme.titleLarge),
-              verticalPadding,
-              ...children
-            ],
-          ),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title, style: Theme.of(context).textTheme.titleLarge),
+            verticalPadding,
+            ...children
+          ],
         ),
       ),
     ));
